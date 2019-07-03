0a8862fb48f78822a8741c75479195a50daf45fc
Michael Mansy <michael.mansy@yahoo.ca>
2019-07-03 13:45:19 -0400

    passing and receiving props

--- src/index.js
+++ src/index.js
@@ -1,58 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return(
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}></img>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}></img>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}></img>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post
-                    </div>
-                </div>
-            </div>
+            < CommentDetail author="Sam" timeAgo="Today at 1:00AM" text="Nice blog post" avatar={faker.image.avatar()}/>
+            < CommentDetail author="Alex" timeAgo="Yesterday at 1:00AM" text="Best Country is Italy" avatar={faker.image.avatar()}/>
+            < CommentDetail author="jerry" timeAgo="Last Monday at 1:00AM" text="Best player is Ronaldo" avatar={faker.image.avatar()}/>
         </div>
     );
 };
